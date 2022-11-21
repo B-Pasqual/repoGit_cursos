99a123e8b1f9fe2cb06ef2000f661c02c2093439
B-Pasqual <brunopasqual0@gmail.com>
2022-11-21 12:19:36 -0300

    array/object destructuring

--- Destructuring/script.js
+++ Destructuring/script.js
@@ -23,6 +23,12 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
 //! Testando Destructuring com valores do objeto
@@ -43,5 +49,28 @@ const restaurant = {
 // console.log(i, j);
 
 //!Object destrucuring ----------------------
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
+
+const { name: nome, openingHours: horas, categories: categorias } = restaurant;
+// console.log(nome, horas, categorias);
+
+//todo Trocando o valor das variáveis com o obj ----
+
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+// ({ a, b } = obj);
+// console.log(a, b);
+
+//todo Nested obj ----
+
+// const {
+//   fri: { aberto, fechado },
+// } = horas;
+// console.log(fri);
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
