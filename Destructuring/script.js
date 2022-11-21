0273b4a8c82616b0403f4eea3edaad4dd5544409
B-Pasqual <brunopasqual0@gmail.com>
2022-11-21 11:27:07 -0300

    array,object destructuring

--- Destructuring/script.js
+++ Destructuring/script.js
@@ -4,6 +4,22 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
@@ -11,12 +27,21 @@ const restaurant = {
 
 //! Testando Destructuring com valores do objeto
 
-let [main, secondary] = restaurant.categories;
-console.log(main, secondary);
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+// let [main, secondary] = restaurant.categories;
+// console.log(main, secondary);
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 
 //todo -- utilizando um método no destructuring
-console.log(restaurant.order(2, 0));
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+// console.log(restaurant.order(2, 0));
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+//todo -- nested destructuring
+// const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+//!Object destrucuring ----------------------
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
