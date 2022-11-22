e840732b3b6c81247b7dfa476779fb21da0838b5
B-Pasqual <brunopasqual0@gmail.com>
2022-11-22 01:39:37 -0300

    spread

---
+++ SpreadOperator/script.js
@@ -0,0 +1,59 @@
+//! Objeto utilizado nas explicações --------------
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+    );
+  },
+};
+
+//copy array
+const newArr = [...restaurant.mainMenu];
+
+// join 2 arrays
+const arrTotal = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(arrTotal);
+
+//Real world example
+const ingredients = [
+  prompt("Let's make pasta! Ingredient 1"),
+  prompt('Ingredient 2?'),
+  prompt('Ingredient 3?'),
+];
+
+restaurant.orderPasta(...ingredients);
+
+//! Criando novo restaurante(objeto), com as propriedades do restaurante original
+
+const newRestaurant = { ...restaurant, fo };
